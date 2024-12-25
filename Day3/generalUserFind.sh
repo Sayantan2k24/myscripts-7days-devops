9f3bd06d54e0b4c63330ea1554042ce52f240100
Sayantan Samanta <90416470+Sayantan2k24@users.noreply.github.com>
2024-12-25 19:03:12 +0530

    Create generalUserFind.sh

---
+++ Day3/generalUserFind.sh
@@ -0,0 +1,11 @@
+[root@localhost day3]# cat generalUserFind.sh 
+for line in `cat /etc/passwd`
+do
+	myid=`echo $line | cut -d: -f3`	
+
+	if [[ $myid -ge 1000 ]]
+ 	then
+		echo $line
+		
+	fi
+done
